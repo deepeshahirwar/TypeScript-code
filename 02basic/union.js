6e44c86ded8e4aa6965a273bd76ef3c0b61f77ec
deepeshahirwar <deepeshahirwar7024@gmail.com>
2024-04-13 04:28:43 +0530

    unioun learn

---
+++ 02basic/union.js
@@ -0,0 +1,8 @@
+//   let  winScore : number| string  ;
+var Deepesh_user = { name: "dde", id: 19798 };
+// deepesh as User 
+//  deepesh as Admin  
+Deepesh_user = {
+    username: "ram",
+    id: 108
+};
---
+++ 02basic/union.ts
@@ -0,0 +1,33 @@
+  
+//   let  winScore : number| string  ;
+
+//  winScore = 10; 
+//  score = "1020" // both are fine 
+  
+
+ type  User = {
+    name : string 
+    id : number 
+ }
+ 
+ type Admin ={
+    username : string 
+    id : number
+ } 
+ 
+
+ var  Deepesh_user : User | Admin =
+ {name : "dde" , id : 19798}
+   
+  // deepesh as User 
+
+//  deepesh as Admin  
+Deepesh_user = {
+    username : "ram",
+    id : 108
+}
+
+ 
+
+    
+
